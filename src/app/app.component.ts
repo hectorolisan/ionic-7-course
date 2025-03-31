a064122b1641626aeef3798109319090d41c16a7
hectorolisan <hectorolisanz@gmail.com>
2025-03-31 16:33:35 +0100

    Version 0.13.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   standalone: false,
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const prefersDark = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    this.setTheme(prefersDark);
+
+    const themeToggleBtn = document.getElementById('themeToggleBtn');
+    if (themeToggleBtn) {
+      themeToggleBtn.addEventListener('click', () => {
+        console.log('Theme toggle button clicked');
+        const isDark = document.body.classList.toggle('dark');
+        this.setTheme(isDark);
+      });
+    }
+  }
+
+  setTheme(isDark: boolean) {
+    const themeIcon = document.getElementById(
+      'themeIcon'
+    ) as HTMLIonIconElement;
+    document.body.classList.toggle('dark', isDark);
+    themeIcon.name = isDark ? 'moon' : 'sunny';
+  }
 }
